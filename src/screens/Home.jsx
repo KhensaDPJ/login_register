e0b14e6271255a5d5b746a2228a56b6467154f22
KhensaDPJ <59077006+KhensaDPJ@users.noreply.github.com>
2023-07-17 13:54:18 +0700

    check token login

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,54 +1,72 @@
-import {View, Text, TouchableOpacity,ScrollView} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  ActivityIndicator,
+} from 'react-native';
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import {HTTP_API} from '@env';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useNavigation} from '@react-navigation/native';
 import Product from '../components/Product';
-import {err} from 'react-native-svg/lib/typescript/xml';
 
 const Home = () => {
   const Navigation = useNavigation();
-  const [ProductData, setProductData] = useState();
-
-  const LogOut = async () => {
-    await AsyncStorage.removeItem('Token');
-    Navigation.navigate('Login', {name: 'Login'});
-  };
+  const [ProductData, setProductData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
 
   const GetProductData = async () => {
-    await axios
-      .get(`${HTTP_API}products`)
-      .then(response => {
-        setProductData(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    try {
+      const response = await axios.get(`${HTTP_API}products`);
+      if (response.data != null) {
+        const data = response.data;
+        setProductData(data);
+        setIsLoading(false);
+      }
+    } catch (err) {
+      console.log(err);
+      const status= err.response.status
+      if(status=='403'){
+        await AsyncStorage.removeItem('Token')
+        Navigation.navigate('Login')
+      }
+    }
   };
 
   useEffect(() => {
     GetProductData();
-  }, [0]);
+  }, []);
 
   return (
     <>
-      <TouchableOpacity onPress={LogOut}>
+      <TouchableOpacity
+        onPress={async () => {
+          await AsyncStorage.removeItem('Token'), Navigation.navigate('Login');
+        }}>
         <Text>LogOut</Text>
       </TouchableOpacity>
-      <ScrollView className='w-full h-screen'>
-      <View className='flex-1 bg-red-500 '>
-      {ProductData.map(data => (
-        <Product
-          key={data._id}
-          title={data.title}
-          detail={data.detail}
-          image={data.image}
-          price={data.price}
-        />
-      ))}
-      </View>
+      <ScrollView className="w-full h-screen">
+        <View className="flex-1">
+          {isLoading ? (
+            <View className="w-full h-screen justify-center bg-white">
+              <ActivityIndicator size="large" color={'gray'} />
+            </View>
+          ) : (
+            ProductData.map(data => (
+              <Product
+                key={data._id}
+                title={data.title}
+                detail={data.detail}
+                image={data.image}
+                price={data.price}
+                // status={data.status}
+              />
+            ))
+          )}
+        </View>
       </ScrollView>
     </>
   );
